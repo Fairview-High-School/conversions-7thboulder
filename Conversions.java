ee130862a198622f6c8388897644c3cf861ce1fb
7thboulder <92888101+7thboulder@users.noreply.github.com>
2024-08-26 20:56:00 +0000

    final

--- Conversions.java
+++ Conversions.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 /**
  * This application converts between various units of measurement.
  *
- * @author Tim Gesell
- * @version 8/21/2022
+ * @author Ezra Old
+ * @version 8/23/2024
  */
 public class Conversions
 {
@@ -17,6 +17,10 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Kilometers to Miles");
+        System.out.println("8. Miles to Kilometers");
+        System.out.println("9. Meters to Miles");
+        System.out.println("10. Miles to meters");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -34,6 +38,75 @@ public class Conversions
         if (selection == 2)
         {
             //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+            System.out.println("Enter Fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double celsius = (fahrenheit - 32) * (5.0/9.0);
+            System.out.println(fahrenheit + " degrees fahrenheit is " + celsius + " degrees celsius");
+        }
+        if (selection == 3)
+        {
+            System.out.println("Enter Feet");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = (feet / 3.281);
+            System.out.println(feet + " feet is " + meters + " meters");
+        }
+        if (selection == 4)
+        {
+            System.out.println("Enter Meters");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = (meters * 3.281);
+            System.out.println(meters + " meters is " + feet + " feet");
+        }
+        if (selection == 5)
+        {
+            System.out.println("Enter Ounces");
+            double ounces = keyboard.nextDouble();
+            keyboard.nextLine();
+            double milliliters = (ounces * 29.574);
+            System.out.println(ounces + " ounces is " + milliliters + " milliliters");
+        }
+        if (selection == 6)
+        {
+            System.out.println("Enter Milliliters");
+            double milliliters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double ounces = (milliliters / 29.574);
+            System.out.println(milliliters + " milliliters is " + ounces + " ounces");
+        }
+        if (selection == 7)
+        {
+            System.out.println("Enter Kilometers");
+            double kilometers = keyboard.nextDouble();
+            keyboard.nextLine();
+            double miles = (kilometers / 1.609);
+            System.out.println(kilometers + " kilometers is " + miles + " miles");
+        }
+        if (selection == 8)
+        {
+            System.out.println("Enter Miles");
+            double miles = keyboard.nextDouble();
+            keyboard.nextLine();
+            double kilometers = (miles * 1.609);
+            System.out.println(miles + " miles is " + kilometers + " kilometers");
+        }
+        if (selection == 9)
+        {
+            System.out.println("Enter Meters");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double miles = (meters / 1609);
+            System.out.println(meters + " meters is " + miles + " miles");
+        }
+        if (selection == 10)
+        {
+            System.out.println("Enter Miles");
+            double miles = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = (miles * 1609);
+            System.out.println(miles + " miles is " + meters + " meters");
         }
         //add more if statements here.
     }
